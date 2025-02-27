1e2a9c99fe1432d501631cc2f675923ac0e2cc44
wadams-dev <wadams.dev@gmail.com>
2025-02-26 22:00:41 -0500

    Passing Data & Keyboard Setup

---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by William Adams on 2/26/25.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -12,6 +12,10 @@ class SearchVC: UIViewController {
     let logoImageView = UIImageView()
     let usernameTextfield = GFTextField()
     let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    var isUsernameEntered: Bool {
+        !usernameTextfield.text!.isEmpty
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +23,29 @@ class SearchVC: UIViewController {
         configureLogoImageView()
         configureUsernameTextfield()
         configureCallToActionButton()
+        createDismissKeyboardTapGesture()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    func createDismissKeyboardTapGesture() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func pushFollowerListVC() {
+        guard isUsernameEntered else {
+            print("No username")
+            return
+        }
+         
+        let followerListVC = FollowerListVC()
+        followerListVC.username = usernameTextfield.text
+        followerListVC.title = usernameTextfield.text
+        navigationController?.pushViewController(followerListVC, animated: true)
     }
     
     func configureLogoImageView() {
@@ -41,6 +63,7 @@ class SearchVC: UIViewController {
     
     func configureUsernameTextfield() {
         view.addSubview(usernameTextfield)
+        usernameTextfield.delegate = self
         
         NSLayoutConstraint.activate([
             usernameTextfield.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
@@ -52,6 +75,7 @@ class SearchVC: UIViewController {
     
     func configureCallToActionButton() {
         view.addSubview(callToActionButton)
+        callToActionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
@@ -61,3 +85,12 @@ class SearchVC: UIViewController {
         ])
     }
 }
+
+extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushFollowerListVC()
+        return true
+    }
+}
+
+
