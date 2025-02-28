d607b739dbda4e16afc0b93242c47578cc383689
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2025-02-28 18:55:00 -0500

    Refactoring error handling using Result Type

--- GHFollowers/Utilities/ErrorMessage.swift
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -5,15 +5,11 @@
 //  Created by William Adams on 2/28/25.
 //
 
-<<<<<<< HEAD
 import UIKit
 
-enum ErrorMessage: String {
+enum GFError: String, Error {
     case invalidUsername = "This username created an invalid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data recieved from the server was invalid. Please try again."
 }
-=======
-import Foundation
->>>>>>> b0e071d7a00e848b724367e47156f35b21a9d2eb
