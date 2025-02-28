b7c50ae07d3e082778f5b00742ebea96ce102791
wadams-dev <wadams.dev@gmail.com>
2025-02-27 20:31:21 -0500

    Implemented follower and user models

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by William Adams on 2/27/25.
+//
+
+import UIKit
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by William Adams on 2/27/25.
+//
+
+import UIKit
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
