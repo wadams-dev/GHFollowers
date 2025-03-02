a3d975eec049c1915d4433a3026a43c8abf988b0
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2025-03-02 00:36:57 -0500

    Finished FollowerCell setup

--- GHFollowers/Custom Views/Cells/FollowerCell.swift
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -13,17 +13,41 @@ class FollowerCell: UICollectionViewCell {
     
     let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    let padding: CGFloat = 8
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        configureAvatarImageView()
+        configureUsernameLabel()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configure() {
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configureAvatarImageView() {
+        addSubview(avatarImageView)
         
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+        ])
+    }
+    
+    private func configureUsernameLabel() {
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
 }
